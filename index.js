5a8f16c86113e54e77f6208c33c9f54fc131514a
Ahmad Awais <me@AhmadAwais.com>
2019-09-19 16:26:57 +0500

    📦 NEW: Stable randomness

--- index.js
+++ index.js
@@ -3,4 +3,22 @@
  *
  * @param {Array} array Data to shuffle.
  */
-export default array => array.sort(() => 0.5 - Math.random());
+module.exports = array => {
+	let currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+};
